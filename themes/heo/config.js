ccdcee71f687556e4eb1c3b4767b33622d239780
hejiawei67 <hejiawei655@gmail.com>
2025-01-08 23:34:00 +0800

    删除交流频道

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,10 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://chem.cnvetman.icu' },
-    { title: '希望你能在此收获一些化学知识', url: 'https://chem.cnvetman.icu' }
+    '欢迎来到我的博客',
+    '希望你能在此收获一些化学知识'
+    // { title: '欢迎来到我的博客', url: 'https://chem.cnvetman.icu' },
+    // { title: '希望你能在此收获一些化学知识', url: 'https://chem.cnvetman.icu' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -21,8 +23,8 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享化学',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'chem.cnvetman.icu',
-  HEO_HERO_TITLE_4: ' ',
-  HEO_HERO_TITLE_5: 'BasicChem 轻松学习高中化学',
+  HEO_HERO_TITLE_4: 'EasyChem ',
+  HEO_HERO_TITLE_5: '轻松学习高中化学',
   HEO_HERO_TITLE_LINK: 'https://chem.cnvetman.icu',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
@@ -53,7 +55,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/hejiawei67',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://chem.cnvetman.icu/about',
+  HEO_INFO_CARD_URL3: '/about',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -116,11 +118,11 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  // HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  // HEO_SOCIAL_CARD_TITLE_1: '交流频道',
+  // HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
+  // HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
+  // HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
